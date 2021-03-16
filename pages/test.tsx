948952b0461937e7417325e3273c41556f42cc5c
frankagathos <frantzeskosagathos@gmail.com>
2021-03-17 00:07:51 +0200

    added getInitial props to fetch data

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,7 +1,21 @@
+import { NextPage } from 'next'
 import React from 'react'
 
-export default function Test() {
+interface Props {
+    stars: number
+}
+
+const Test: NextPage<Props> = ({ stars }) => {
     return (
-        <div>Test page</div>
+        <div>Github stars {stars}</div>
     )
 }
+
+Test.getInitialProps = async () => {
+    const res = await fetch('https://api.github.com/repos/frankagathos/gatsby-with-php-form')
+    const json = await res.json()
+    return { stars: json.stargazers_count }
+}
+
+
+export default Test
