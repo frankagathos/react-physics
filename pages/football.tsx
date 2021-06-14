36696464319ea3bd78dad815952ecf6acff229dc
frankagathos <frantzeskosagathos@gmail.com>
2021-06-15 00:41:30 +0300

    added football api fetch example

--- pages/football.tsx
+++ pages/football.tsx
@@ -1,16 +1,21 @@
 import React, { useState } from 'react'
 import Head from 'next/head'
 import { useEffect } from 'react';
+import {
+    Text
+} from '@chakra-ui/react';
+import { List, ListItem, ListIcon, OrderedList, UnorderedList, Heading } from "@chakra-ui/react"
 
-
-const Cubes = () => {
-
-    const [data,setData] = useState(null)
+const Football = () => {
+    const authToken = '6417a0e1c04349f0884be2088bd27d91';
+    const [data, setData] = useState<null | any>(null)
 
     useEffect(() => {
-        fetch('https://api.football-data.org/v2/competitions/SA/scorers',{ headers: {
-            'X-Auth-Token': '6417a0e1c04349f0884be2088bd27d91'
-          }})   
+        fetch('https://api.football-data.org/v2/competitions/SA/scorers', {
+            headers: {
+                'X-Auth-Token': authToken
+            }
+        })
             .then(response => response.json())
             .then(data => setData(data));
     }, [])
@@ -20,10 +25,22 @@ const Cubes = () => {
             <Head>
                 <meta name="description" content="Fetch data from football api" />
             </Head>
-            {data?.scorers[0].numberOfGoals}
+            <Heading>Italy's top scorers</Heading>
+            <br></br>
+            {data &&
+
+                <UnorderedList>
+                    {data.scorers.map(scorer => {
+                        return (
+                            <ListItem><span>{scorer.player.name}</span>  has <span>{scorer.numberOfGoals}</span></ListItem>
+                        )
+                    })
+                    }
+                </UnorderedList>}
+
         </>
     )
 }
 
 
-export default Cubes
+export default Football
