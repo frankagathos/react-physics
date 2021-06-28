5d39c3c829824b7a3b1c357edce418df11246e20
frankagathos <frantzeskosagathos@gmail.com>
2021-06-28 15:53:16 +0300

    text in fetch github example

--- pages/fetch-github.tsx
+++ pages/fetch-github.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from 'next'
 import React from 'react'
 import { Heading, Text, Container } from "@chakra-ui/react"
+import Text3d from '../components/Text3d'
 
 interface Props {
     name: string
@@ -13,15 +14,14 @@ const FetchGithub: NextPage<Props> = ({ name, stars }) => {
             <Heading>Fetch data from github api</Heading>
             <Text>Example fetching data with SSR</Text>
             <br></br>
-            <div>Project: {name}</div>
-            <div>Github stars: {stars}</div>
+            <Text3d fontSize={15} textCopy={`${name}, has ${stars}.`}/>
         </Container>
     )
 }
 
 
 export async function getServerSideProps(context) {
-    const res = await fetch('https://api.github.com/repos/frankagathos/gatsby-with-php-form')
+    const res = await fetch('https://api.github.com/repos/mrdoob/three.js/')
     const json = await res.json()
     return {
         props: { name: json.name, stars: json.stargazers_count }, // will be passed to the page component as props
