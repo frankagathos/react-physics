8a396e54484e322c75c8217d0c1e60d8fa45cdf4
Frank Agathos <frankagathos@gmail.com>
2022-04-16 22:42:08 +0100

    Add grid on box

--- pages/examples/shooting.tsx
+++ pages/examples/shooting.tsx
@@ -2,10 +2,8 @@ import { NextPage, NextPageContext } from 'next'
 import React, { Suspense, useEffect, useRef } from 'react'
 import { Heading, Text, Container } from '@chakra-ui/react'
 import { Canvas } from '@react-three/fiber'
-import Box from '../../components/objects/box'
 import * as THREE from 'three'
-import { useLoader } from '@react-three/fiber'
-import useEventListener from '../../hooks/useEventListener'
+import { OrbitControls } from '@react-three/drei'
 
 // Need start creating a shooting game
 
@@ -13,17 +11,31 @@ interface Props {}
 
 const Shoot: NextPage<Props> = () => {
   const container = useRef()
-  const pointer = new THREE.Vector2()
-  // useEventListener(container, 'click', (e: any) => console.log('move'+e))
   return (
     <Container>
-      {/* <Heading>Model</Heading> */}
-      {/* <Text>model</Text> */}
       {/*  @ts-ignore */}
       <Canvas ref={container}>
-        <Box position={[-1.2, 0, 0]} />
-        <Box position={[0, 0, 0]} />
-        <Box position={[1.2, 0, 0]} />
+        <ambientLight />
+        <OrbitControls
+          enableZoom={true}
+          enablePan={false}
+          enableDamping
+          dampingFactor={0.1}
+          autoRotate={false}
+          rotateSpeed={-0.15}
+        />
+        {/* <mesh position={new THREE.Vector3(0, 10, 0)}>
+          <gridHelper args={[10, 100]} />
+          <meshPhongMaterial side={THREE.DoubleSide} attach="material" />
+        </mesh> */}
+        <mesh>
+          <gridHelper
+            args={[10,10]}
+            position={new THREE.Vector3(0, 0.5, 0)}
+          />
+          <boxGeometry args={[10, 1, 10]} />
+          <meshPhongMaterial side={THREE.DoubleSide} attach="material" />
+        </mesh>
       </Canvas>
     </Container>
   )
