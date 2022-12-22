f8e20f231c022c8d0c220bcb4170032b62663df8
Frank Agathos <frankagathos@gmail.com>
2022-12-22 22:33:33 +0000

    ignore linting errors during build

--- next.config.js
+++ next.config.js
@@ -6,6 +6,11 @@ module.exports = {
     // !! WARN !!
     ignoreBuildErrors: false,
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   webpack: (config, options) => {
     config.module.rules.push({
       test: /\.(glb|gltf)$/,
