8aed8e533949d3696635992a74aab3da0223fff0
Frank Agathos <frankagathos@gmail.com>
2022-12-24 17:55:00 +0000

    add earth

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,34 @@
+//.eslintrc.js
 module.exports = {
-  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@next/next/recommended',
+  ],
   parserOptions: {
-    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module', // Allows for the use of imports
     ecmaFeatures: {
-      jsx: true, // Allows for the parsing of JSX
+      jsx: true,
     },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/no-unknown-property': ['error', { ignore: ['jsx'] }],
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+  globals: {
+    React: 'writable',
   },
   settings: {
     react: {
-      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+      version: 'detect',
     },
-  },
-  extends: [
-    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
-    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
-  ],
-  rules: {
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-    '@typescript-eslint/ban-ts-ignore': 'off',
   },
 }
