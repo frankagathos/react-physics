16524d2104d34c4d3d103cfad4868013b081efd1
Frank Agathos <frankagathos@gmail.com>
2022-12-30 19:49:15 +0000

    fixed controls

--- pages/game.tsx
+++ pages/game.tsx
@@ -3,34 +3,29 @@ import { Canvas, useFrame } from '@react-three/fiber'
 import { NextPage } from 'next'
 import * as THREE from 'three'
 import { DoubleSide } from 'three'
-import { Physics, useBox, usePlane } from '@react-three/cannon'
+import { Physics, useBox } from '@react-three/cannon'
 import { Trex } from './examples/t-rex'
 import usePersonControls from '../hooks/usePersonControls'
 
-function Plane({ ...props }: any) {
-  const [ref] = usePlane(() => ({
+function BoxPlane({ ...props }: any) {
+  const [ref] = useBox(() => ({
     type: 'Static',
-    rotation: [Math.PI / 2, 0, 0],
-    // fixedRotation: true,
+    rotation: [-Math.PI / 2, 0, 0],
+    args: [50, 50, 1],
     ...props,
   }))
   return (
-    <mesh
-      ref={ref}
-      receiveShadow
-      //  rotation={new THREE.Euler(Math.PI / 2, 0, 0)}
-    >
-      {/* <gridHelper args={[10, 10]} position={new THREE.Vector3(0, 0.5, 0)} /> */}
-      <planeBufferGeometry args={[50, 50]} />
-      <meshPhongMaterial side={DoubleSide} color={'white'} />
+    <mesh ref={ref} receiveShadow>
+      <boxBufferGeometry args={[50, 50, 1]} attach="geometry" />
+      <meshLambertMaterial attach="material" color="white" />
     </mesh>
   )
 }
 
 const TrexMesh = () => {
   const [ref, api] = useBox(() => ({
-    // mass: 10,
-    // isKinematic: true,
+    mass: 1,
+    isKinematic: true,
     args: [1, 1, 1],
     position: [0, 5, 0],
     type: 'Dynamic',
@@ -48,9 +43,9 @@ const TrexMesh = () => {
     let downVector = new THREE.Vector3(0, 0, 0)
     let frontSideVector = new THREE.Vector3(0, 0, 0)
 
-    downVector.set(0, -Number(downward), 0)
-    frontVector.set(0, 0, Number(forward) - Number(backward))
-    sideVector.set(Number(right) - Number(left), 0, 0)
+    downVector.set(0, Number(downward), 0)
+    frontVector.set(0, 0, -Number(forward) + Number(backward))
+    sideVector.set(-Number(right) + Number(left), 0, 0)
 
     direction
       .subVectors(frontVector, sideVector)
@@ -74,26 +69,18 @@ const TrexMesh = () => {
 }
 
 const Game: NextPage = () => {
-  // useFrame(() => {
-
-  // })
-
   return (
     <Canvas
       gl={{ alpha: false }}
       camera={{
-        // position: [0, 50, 50],
         position: [0, 15, 25],
-        // rotation: new THREE.Euler(0, Math.PI, 0),
-        // rotation: [Math.PI / 2, 0, 0],
         isPerspectiveCamera: true,
-        // rotateX:(camera)=> camera.rotation.set(deg2rad(30), 0, 0);
       }}
     >
       <axesHelper position={new THREE.Vector3(0, 0, 0)} args={[5]} />
-      <Physics debug gravity={[0, 0, 0]}>
+      <Physics gravity={[0, -100, 0]} debug>
         <TrexMesh />
-        <Plane />
+        <BoxPlane />
       </Physics>
       <OrbitControls />
       <ambientLight intensity={0.8} />
